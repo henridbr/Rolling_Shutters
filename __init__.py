2bf9a2b0a0e271ea143e9a04b3f2c292397bc6b1
henridbr <henridbr@users.noreply.github.com>
2018-10-28 13:24:46 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,21 +1,4 @@
-# Copyright 2016 Mycroft AI, Inc.
-#
-# This file is part of Mycroft Core.
-#
-# Mycroft Core is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Mycroft Core is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
-
-# SWK_rolling_shutters
+# Rolling_Shutters
 # Mycroft <-> Wemos mini
 # ESP8266_IP_address : http://RS_IpAddress/
 # command : /?up
@@ -44,7 +27,7 @@ class RollingShuttersSkill(MycroftSkill):
         super(RollingShuttersSkill, self).__init__(name="RollingShuttersSkill")
     
         self.Ip_Address = self.settings.get('RS_IpAddress') 
-        print("Ip address : ",self.Ip_Address)
+  #      print("Ip address : ",self.Ip_Address)
         
     @intent_handler(IntentBuilder("OpenShuttersIntent").require("OpenShuttersKeyword"))
     def handle_open_shutters_intent(self, message):
